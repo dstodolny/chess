b2bf55dfd800fe26f60a2555574e0cfa723a3d15
Dominik Stodolny <d.stodolny@gmail.com>
2015-02-08 19:54:21 +0100

    write player spec

--- lib/chess/player.rb
+++ lib/chess/player.rb
@@ -1,5 +1,10 @@
 module Chess
   class Player
-
+    attr_accessor :name, :color
+    
+    def initialize(input)
+      @name = input.fetch(:name)
+      @color = input.fetch(:color)
+    end
   end
 end
